6df8e934498f9123f515451b92c7c10de3dc81ef
Axel <axel.duval74@gmail.com>
2020-11-05 20:01:24 +0100

    More setup

--- server/v1/index.js
+++ server/v1/index.js
@@ -8,9 +8,23 @@ const root = (req, res)=>{
 	res.status(200).json({message:  "your are on root"})
 }
 
-// GET main section
+/**
+* GET questions
+*/
+const questions = (req, res)=>{
+	/* Get params */
+	const count = req.query.count,
+	const pronounced = req.query.pronounced;
+	
+	//TODO get asked data and return it
+	res.status(200).json({message:  "your are on questions"})
+}
+
+// GET -> /api/v1/
 router.get('/', root)
 
+// GET -> /api/v1/questions
+router.get('/questions', questions)
 
 
 module.exports = router
