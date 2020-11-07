bc6202269ce2d470010a1d5df88d56e9c4a60f42
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-11-07 14:43:49 +0100

    Add endpoint to get random questions to common interfaces

--- common/src/api.ts
+++ common/src/api.ts
@@ -1,10 +1,22 @@
 export namespace endpoints {
 
-    export namespace GET_RANDOM_QUESTION {
+    export namespace GET_RANDOM_QUOTES {
         export const method = 'GET';
-        export const endpoint = '/question';
+        export const endpoint = (count?: number) => `/api/quotes?count=${count || 1}`;
+        export const expressEndpoint = '/api/quotes';
+
+        interface Quote {
+            quote: string;
+            prounonced: boolean;
+            answers: {
+                correct: number;
+                incorrect: number;
+            },
+            sources: string[];
+        }
+
         export interface Response {
-            message: string;
+            quotes: Quote[]
         }
     }
 
