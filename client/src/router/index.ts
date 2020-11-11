8845269c9dd6ad757de3d36c050d31cd6675901f
Axel <axel.duval74@gmail.com>
2020-11-11 18:58:04 +0100

    og:meta

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -6,12 +6,30 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      meta: [
+        { property: 'og:title',     content: "Did Trump ever say that?"},
+        { property: 'og:description', content: 'Did really Trump say that the concept of global warming was created by and for the Chinese? Yes, of course. Want to know more? Take the test.'},
+        { property: 'og:site_name', content: 'Did Trump Say'},
+        { property: 'og:type',      content: 'website'},
+        { property: 'og:image',     content: '../assets/logo.png'}
+      ]
+    }
   },
   {
     path: '/play/',
     name: 'Play',
-    component: Play
+    component: Play,
+    meta: {
+      meta: [
+        { property: 'og:title',     content: "Let's Play!"},
+        { property: 'og:description', content: 'I dare you to beat me at this quiz.'},
+        { property: 'og:site_name', content: 'Did Trump Say'},
+        { property: 'og:type',      content: 'website'},
+        { property: 'og:image',     content: '../assets/logo.png'}
+      ]
+    }
   },
   {
     path: '/:catchAll(.*)',
